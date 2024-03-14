2c9b6e795b17545b80d95d541a3f3b3128e05210
Quarmex <quarmex@romecube.ru>
2024-03-14 20:48:23 +0300

    Add files via upload

---
+++ 404.php
@@ -0,0 +1,78 @@
+<?php
+header('HTTP/1.1 404 Not Found');
+?>
+<!doctype html>
+<html lang="en" data-bs-theme="auto">
+  <head><script src="../assets/js/color-modes.js"></script>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="Quarmex">
+
+    <title>Althernania » Страница не найдена</title>
+
+    <link rel="canonical" href="https://althernania.github.io/">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="icon" type="image/x-icon" href="icon.png">
+  </head>
+  <body>
+<main>
+  <header class="p-3 text-bg-dark">
+    <div class="container">
+      <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
+        <a href="https://althernania.github.io/" class="d-flex align-items-center mb-2 mb-lg-0 text-white text-decoration-none">
+          <img class="bi me-2" width="40" height="40" role="img" aria-label="Bootstrap" src="icon.png">
+        </a>
+
+        <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
+          <li><a href="https://althernania.github.io/projects/" class="nav-link px-2 text-white">Проекты</a></li>
+          <li><a href="https://althernania.github.io/contact/" class="nav-link px-2 text-white">Контакты</a></li>
+          <li><a href="https://althernania.github.io/stockholders/" class="nav-link px-2 text-white">Акционерам</a></li>
+          <li><a href="https://althernania.github.io/partners/" class="nav-link px-2 text-white">Партнёрам</a></li>
+          <li><a href="https://althernania.github.io/team/" class="nav-link px-2 text-white">Команда</a></li>
+        </ul>
+
+        <form class="col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3" role="search">
+          <input type="search" class="form-control form-control-dark text-bg-dark" placeholder="Поисковая система..." aria-label="Search">
+        </form>
+
+        <div class="text-end">
+          <button onclick="window.location.href='https://althernania.github.io/vk/';" type="button" class="btn btn-outline-light me-2">ВКонтакте</button>
+          <button onclick="window.location.href='https://althernania.github.io/telegram/';" type="button" class="btn btn-warning">Telegram</button>
+        </div>
+      </div>
+    </div>
+  </header>
+  <div class="px-4 py-5 my-5 text-center">
+    <img class="d-block mx-auto mb-4" src="icon.png" alt="" width="72" height="72">
+    <h1 class="display-5 fw-bold text-body-emphasis">» Ошибка 404 «</h1>
+    <div class="col-lg-6 mx-auto">
+      <p class="lead mb-4">» 🔥 Страница не найдена! «</p>
+      <div class="d-grid gap-2 d-sm-flex justify-content-sm-center">
+        <button onclick="window.location.href='https://althernania.github.io/';" type="button" class="btn btn-warning btn-lg px-4 gap-3">Главная</button>
+      </div>
+    </div>
+  </div>
+  <div class="container">
+  <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
+    <p class="col-md-4 mb-0 text-body-secondary">2023 &copy; Althernania</p>
+
+    <a href="https://althernania.github.io/" class="col-md-4 d-flex align-items-center justify-content-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none">
+      <img src="icon.png" class="bi me-2" width="40" height="40">
+    </a>
+
+    <ul class="nav col-md-4 justify-content-end">
+      <li class="nav-item"><a href="https://althernania.github.io/projects/" class="nav-link px-2 text-body-secondary">Проекты</a></li>
+      <li class="nav-item"><a href="https://althernania.github.io/contact/" class="nav-link px-2 text-body-secondary">Контакты</a></li>
+      <li class="nav-item"><a href="https://althernania.github.io/stockholders/" class="nav-link px-2 text-body-secondary">Акционерам</a></li>
+      <li class="nav-item"><a href="https://althernania.github.io/partners/" class="nav-link px-2 text-body-secondary">Партнёрам</a></li>
+      <li class="nav-item"><a href="https://althernania.github.io/team/" class="nav-link px-2 text-body-secondary">Команда</a></li>
+    </ul>
+  </footer>
+</div>
+</main>
+<script src="../assets/dist/js/bootstrap.bundle.min.js"></script>
+
+    </body>
+</html>
